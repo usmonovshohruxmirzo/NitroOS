c3d9a3cf1c382d121756c64543d656ace9dbf1fb
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-02 23:45:26 +0500

    feat: add interactive button at (10, 10) with 100x40 green fill,  text, and toggle red/blue rectangle color on click with 15x15 black cursor

--- NitroOS/Kernel.cs
+++ NitroOS/Kernel.cs
@@ -1,24 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Cosmos.System;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using System;
+using System.Drawing;
 using Sys = Cosmos.System;
 
 namespace NitroOS
 {
     public class Kernel : Sys.Kernel
     {
+        private Canvas canvas;
+        private Pen redPen;
+        private Pen cursor;
+        private Pen buttonPen;
+        private bool isRed = true;
+        private const int buttonX = 10;
+        private const int buttonY = 10;
+        private const int buttonWidth = 100;
+        private const int buttonHeight = 40;
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Welcome to Nitro OS");
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(800, 600, ColorDepth.ColorDepth32));
+            canvas.Clear(Color.White);
+
+            redPen = new Pen(Color.Red);
+            cursor = new Pen(Color.Black);
+            buttonPen = new Pen(Color.Green);
+
+            MouseManager.ScreenWidth = 800;
+            MouseManager.ScreenHeight = 600;
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            canvas.DrawFilledRectangle(new Pen(Color.White), 0, 0, 800, 600);
+
+            canvas.DrawFilledRectangle(buttonPen, buttonX, buttonY, buttonWidth, buttonHeight);
+            canvas.DrawString(
+                "Click Me",
+                PCScreenFont.Default,
+                new Pen(Color.Black),
+                buttonX + 5,
+                buttonY + 10
+            );
+
+            if (MouseManager.MouseState == MouseState.Left &&
+                MouseManager.X >= buttonX && MouseManager.X <= buttonX + buttonWidth &&
+                MouseManager.Y >= buttonY && MouseManager.Y <= buttonY + buttonHeight)
+            {
+                isRed = !isRed;
+                System.Threading.Thread.Sleep(200);
+            }
+
+            canvas.DrawFilledRectangle(new Pen(isRed ? Color.Red : Color.Blue), 200, 150, 400, 300);
+
+            canvas.DrawFilledRectangle(cursor, (int)MouseManager.X, (int)MouseManager.Y, 15, 15);
+
+            canvas.Display();
         }
     }
 }
--- NitroOS/obj/Debug/net6.0/NitroOS.AssemblyInfo.cs
+++ NitroOS/obj/Debug/net6.0/NitroOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Use VMware Player or Workstation to deploy and debug.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+034f0563fa33edc960976cf51ca55b2b6e031574")]
 [assembly: System.Reflection.AssemblyProductAttribute("NitroOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NitroOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
