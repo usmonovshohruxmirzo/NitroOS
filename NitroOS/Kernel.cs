034f0563fa33edc960976cf51ca55b2b6e031574
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-02 21:51:24 +0500

    init: setup Nitro OS

---
+++ NitroOS/Kernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace NitroOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to Nitro OS");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
