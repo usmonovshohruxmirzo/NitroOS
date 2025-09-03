6f8b739a782b65e5c346bc7ee633e910f8effa4c
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-03 23:19:55 +0500

    refactor(Kernel): add apppanel, taskbar, menubutton, calculator & paint app
    - implemented shutdown button
    - add settings & wallpapers item to menu
    - added new rbg color called primaryPen
    - removed test button which is "click me" button

--- NitroOS/Kernel.cs
+++ NitroOS/Kernel.cs
@@ -1,59 +1,98 @@
 ﻿using Cosmos.System;
 using Cosmos.System.Graphics;
 using Cosmos.System.Graphics.Fonts;
+using NitroOS.UI;
+using NitroOS.UI.Taskbar;
 using System;
 using System.Drawing;
 using Sys = Cosmos.System;
+using NitroOS.App;
 
 namespace NitroOS
 {
     public class Kernel : Sys.Kernel
     {
         private Canvas canvas;
-        private Pen redPen;
+        private Pen primaryPen;
         private Pen cursor;
-        private Pen buttonPen;
-        private bool isRed = true;
-        private const int buttonX = 10;
-        private const int buttonY = 10;
-        private const int buttonWidth = 100;
-        private const int buttonHeight = 40;
+        private Taskbar taskbar;
 
         protected override void BeforeRun()
         {
-            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(800, 600, ColorDepth.ColorDepth32));
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(1024, 768, ColorDepth.ColorDepth32));
             canvas.Clear(Color.White);
 
-            redPen = new Pen(Color.Red);
+            primaryPen = new Pen(Color.FromArgb(0xFF, 0xFC, 0x48, 0x50));
             cursor = new Pen(Color.Black);
-            buttonPen = new Pen(Color.Green);
 
-            MouseManager.ScreenWidth = 800;
-            MouseManager.ScreenHeight = 600;
+            // -------------------
+            // Menu items
+            // --------------------------
+            AppsPanel appsCenter = null;
+
+            var settingsItem = new TaskbarButton(0, 0, 180, 35, "Settings", new Pen(Color.Gray), new Pen(Color.White),
+                () =>
+                {
+                    appsCenter.AddWindow(new Window(200, 150, 400, 300, new Pen(Color.DarkCyan), "Settings"));
+                });
+
+            var wallpaperItem = new TaskbarButton(0, 0, 180, 35, "Wallpapers", new Pen(Color.Gray), new Pen(Color.White),
+              () =>
+              {
+                  appsCenter.AddWindow(new Window(200, 150, 400, 300, new Pen(Color.DarkCyan), "Wallpapers"));
+              });
+
+            var shutdownItem = new TaskbarButton(0, 0, 180, 35, "Shutdown", new Pen(Color.Gray), new Pen(Color.White),
+              () => Power.Shutdown());
+
+            // --------------------------
+            // Menu (opens above taskbar)
+            // --------------------------
+            var menu = new Menu(10, 768 - 70, 200, primaryPen, new TaskbarButton[] { shutdownItem, settingsItem, wallpaperItem });
+
+            // --------------------------
+            // Left taskbar button
+            // --------------------------
+            var menuButton = new TaskbarButton(10, 768 - 70 + 15, 100, 40, "Nitro OS", new Pen(Color.White), primaryPen,
+                () => { menu.IsOpen = !menu.IsOpen; });
+
+            // --------------------------
+            // Center apps (optional)
+            // --------------------------
+            var calcButton = new TaskbarButton(400, 768 - 70 + 10, 50, 50, "Calc",
+                new Pen(Color.Green), new Pen(Color.White),
+                () =>
+                {
+                    var calcApp = new CalculatorApp(250, 200, 250, 250);
+                    appsCenter.AddWindow(calcApp.CalcWindow);
+                });
+            var paintButton = new TaskbarButton(460, 768 - 70 + 10, 50, 50, "Paint",
+                new Pen(Color.Blue), new Pen(Color.White),
+                () =>
+                {
+                    var paintApp = new PaintApp(250, 200, 400, 300);
+                    appsCenter.AddWindow(paintApp.PaintWindow);
+                });
+
+            appsCenter = new AppsPanel(400, 768 - 70 + 10, 120, 50, new TaskbarButton[] { calcButton, paintButton });
+
+            // --------------------------
+            // Create taskbar
+            // --------------------------
+            taskbar = new Taskbar(1024, 768, 70, primaryPen, new TaskbarButton[] { menuButton }, new Menu[] { menu }, appsCenter);
+
+            MouseManager.ScreenWidth = 1024;
+            MouseManager.ScreenHeight = 768;
         }
 
         protected override void Run()
         {
-            canvas.DrawFilledRectangle(new Pen(Color.White), 0, 0, 800, 600);
+            canvas.DrawFilledRectangle(new Pen(Color.White), 0, 0, 1024, 768);
 
-            canvas.DrawFilledRectangle(buttonPen, buttonX, buttonY, buttonWidth, buttonHeight);
-            canvas.DrawString(
-                "Click Me",
-                PCScreenFont.Default,
-                new Pen(Color.Black),
-                buttonX + 5,
-                buttonY + 10
-            );
+            taskbar.Draw(canvas);
+            taskbar.CheckClicks();
 
-            if (MouseManager.MouseState == MouseState.Left &&
-                MouseManager.X >= buttonX && MouseManager.X <= buttonX + buttonWidth &&
-                MouseManager.Y >= buttonY && MouseManager.Y <= buttonY + buttonHeight)
-            {
-                isRed = !isRed;
-                System.Threading.Thread.Sleep(200);
-            }
-
-            canvas.DrawFilledRectangle(new Pen(isRed ? Color.Red : Color.Blue), 200, 150, 400, 300);
+            taskbar.AppsCenter.Update(canvas);
 
             canvas.DrawFilledRectangle(cursor, (int)MouseManager.X, (int)MouseManager.Y, 15, 15);
 
