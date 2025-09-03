554a30608f2b406f8cc381aa11a98b85875e743d
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-03 23:15:45 +0500

    feat(Taskbar): add Taskbar container & taskbar button

---
+++ NitroOS/UI/Taskbar/Taskbar.cs
@@ -0,0 +1,58 @@
+﻿using Cosmos.System;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace NitroOS.UI.Taskbar
+{
+    public class Taskbar
+    {
+        public int ScreenWidth, ScreenHeight, Height;
+        public Pen Background;
+        public TaskbarButton[] Buttons;
+        public Menu[] Menus;
+        public AppsPanel AppsCenter;
+
+        public Taskbar(int screenWidth, int screenHeight, int height, Pen bg,
+            TaskbarButton[] buttons, Menu[] menus, AppsPanel appsCenter = null)
+        {
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+            Height = height;
+            Background = bg;
+            Buttons = buttons;
+            Menus = menus;
+            AppsCenter = appsCenter;
+        }
+
+        public void Draw(Canvas canvas)
+        {
+            canvas.DrawFilledRectangle(Background, 0, ScreenHeight - Height, ScreenWidth, Height);
+
+            foreach (var btn in Buttons)
+                btn.Draw(canvas);
+
+            AppsCenter?.Draw(canvas);
+
+            foreach (var menu in Menus)
+                menu.Draw(canvas);
+
+            string dateStr = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+            canvas.DrawString(dateStr, PCScreenFont.Default, new Pen(Color.White),
+                new Cosmos.System.Graphics.Point(ScreenWidth - 200, ScreenHeight - Height + 25));
+        }
+
+        public void CheckClicks()
+        {
+            foreach (var btn in Buttons)
+                btn.CheckClick();
+
+            AppsCenter?.CheckClicks();
+
+            foreach (var menu in Menus)
+                menu.CheckClicks();
+        }
+    }
+}
---
+++ NitroOS/UI/Taskbar/TaskbarButton.cs
@@ -0,0 +1,48 @@
+﻿using Cosmos.System;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroOS.UI.Taskbar
+{
+    public class TaskbarButton
+    {
+        public int X, Y, Width, Height;
+        public string Text;
+        public Pen Background, TextColor;
+        public Action OnClick;
+
+        public TaskbarButton(int x, int y, int width, int height, string text, Pen bg, Pen fg, Action onClick)
+        {
+            X = x; Y = y; Width = width; Height = height;
+            Text = text; Background = bg; TextColor = fg; OnClick = onClick;
+        }
+
+        public void Draw(Canvas canvas)
+        {
+            bool mouseOver = MouseManager.X >= X && MouseManager.X <= X + Width &&
+                             MouseManager.Y >= Y && MouseManager.Y <= Y + Height;
+
+            var bgPen = mouseOver ? new Pen(Color.FromArgb(180, Background.Color)) : Background;
+
+            canvas.DrawFilledRectangle(bgPen, X, Y, Width, Height);
+            canvas.DrawString(Text, PCScreenFont.Default, TextColor, new Cosmos.System.Graphics.Point(X + 5, Y + 10));
+        }
+
+        public void CheckClick()
+        {
+            if (MouseManager.MouseState == MouseState.Left &&
+                MouseManager.X >= X && MouseManager.X <= X + Width &&
+                MouseManager.Y >= Y && MouseManager.Y <= Y + Height)
+            {
+                OnClick?.Invoke();
+            }
+        }
+    }
+
+}
